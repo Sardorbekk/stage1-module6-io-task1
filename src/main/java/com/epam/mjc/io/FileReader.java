849e29ba3ad9ca35367a416545a282d4650abc45
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-03-06 14:32:28 +0500

    hi

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,29 +1,46 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-import java.util.HashMap;
+
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
+    public Profile getDataFromFile(File file)  {
+        DataInputStream in = null;
+        BufferedReader out = null;
         try {
 
-            FileInputStream fstream=new FileInputStream("C:\\Users\\Sardor.me\\IdeaProjects\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
-            DataInputStream in = new DataInputStream(fstream);
-            BufferedReader out=new BufferedReader(new InputStreamReader(in));
+            FileInputStream fstream=new FileInputStream(file);
+             in = new DataInputStream(fstream);
+             out=new BufferedReader(new InputStreamReader(in));
             String strLine;
 
 
             while ((strLine=out.readLine())!=null){
                 System.out.println(strLine);
             }
-         in.close();
-
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+
+            e.getMessage();
         }
-        return new Profile();
+        finally {
+
+
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return new  Profile();
     }
 }
