5bb89d697fd5b83ed533fd3f93f6e91e5c449589
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-03-07 20:09:10 +0500

    hi

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,16 +6,16 @@ import java.io.*;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        BufferedReader out = null;
         try {
             FileInputStream fstream = new FileInputStream(file);
             DataInputStream in = new DataInputStream(fstream);
-            BufferedReader out = new BufferedReader(new InputStreamReader(in));
+            out = new BufferedReader(new InputStreamReader(in));
 
             String nameLine = out.readLine();
             String ageLine = out.readLine();
             String emailLine = out.readLine();
             String phoneLine = out.readLine();
-
             String name = nameLine.split(": ")[1];
             Integer age = Integer.parseInt(ageLine.split(": ")[1]);
             String email = emailLine.split(": ")[1];
@@ -25,6 +25,12 @@ public class FileReader {
             return new Profile(name, age, email, phone);
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return new Profile();
     }
