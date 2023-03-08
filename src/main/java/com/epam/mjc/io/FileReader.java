827ebffed5863c2ff2576b996bc7ea170a2e19be
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-03-08 09:43:15 +0500

    hi

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,10 +1,6 @@
 package com.epam.mjc.io;
-
 import java.io.*;
-
-
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
           try(java.io.FileReader fileReader=new java.io.FileReader(file);
               BufferedReader bufferedReader=new BufferedReader(fileReader)
@@ -17,61 +13,10 @@ public class FileReader {
               Integer age = Integer.parseInt(ageLine.split(": ")[1]);
               String email = emailLine.split(": ")[1];
               Long phone = Long.parseLong(phoneLine.split(": ")[1]);
-
               return new Profile(name, age, email, phone);
-
-
-          } catch (FileNotFoundException e) {
-              e.printStackTrace();
           } catch (IOException e) {
               e.printStackTrace();
           }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*try {
-             fstream = new FileInputStream(file);
-             in = new DataInputStream(fstream);
-            out = new BufferedReader(new InputStreamReader(in));
-
-            String nameLine = out.readLine();
-            String ageLine = out.readLine();
-            String emailLine = out.readLine();
-            String phoneLine = out.readLine();
-            String name = nameLine.split(": ")[1];
-            Integer age = Integer.parseInt(ageLine.split(": ")[1]);
-            String email = emailLine.split(": ")[1];
-            Long phone = Long.parseLong(phoneLine.split(": ")[1]);
-
-            return new Profile(name, age, email, phone);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                 in.close();
-                out.close();
-            } catch (NullPointerException | IOException e) {
-                e.printStackTrace();
-            }
-        }*/
-
         return new Profile();
     }
     }
